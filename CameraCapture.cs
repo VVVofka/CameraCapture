764880b288c831524489738119e4e4fc5a88c0bc
vvvofka@yandex.ru <vvvofka@yandex.ru>
2019-06-15 10:27:45 +0300

    Revert "Before delete 3 imageBox"
    This reverts commit a5c52923491e930ff65912f264c59a254c8f4cba.

--- CameraCapture.cs
+++ CameraCapture.cs
@@ -40,17 +40,26 @@ namespace CameraCapture {
 			_smallGrayFrame = new Mat();
 			_smoothedGrayFrame = new Mat();
 			_cannyFrame = new Mat();
-		} // ///////////////////////////////////////////////////////////////////////////////////////////////
+		}
+
 		private void ProcessFrame(object sender, EventArgs arg) {
 			if (_capture != null && _capture.Ptr != IntPtr.Zero) {
 				_capture.Retrieve(_frame, 0);
 
 				CvInvoke.CvtColor(_frame, _grayFrame, ColorConversion.Bgr2Gray);
+
 				CvInvoke.PyrDown(_grayFrame, _smallGrayFrame);
+
 				CvInvoke.PyrUp(_smallGrayFrame, _smoothedGrayFrame);
+
 				CvInvoke.Canny(_smoothedGrayFrame, _cannyFrame, 100, 60);
 				Run(_frame);
 				captureImageBox.Image = _frame;
+
+				grayscaleImageBox.Image = _grayFrame;
+				smoothedGrayscaleImageBox.Image = _smoothedGrayFrame;
+				cannyImageBox.Image = _cannyFrame;
+				//MyCaptur();
 			}
 		} // /////////////////////////////////////////////////////////////////////////////////////////////
 		private void captureButtonClick(object sender, EventArgs e) {
@@ -79,14 +88,30 @@ namespace CameraCapture {
 				_capture.FlipVertical = !_capture.FlipVertical;
 		} // /////////////////////////////////////////////////////////////////////////////////////////////////////////
 		private void Run(UMat image) { //!!!UMat
+			
+			//IImage image;
+			//Read the files as an 8-bit Bgr image  
+
+			//image = new UMat("lena.jpg", ImreadModes.Color); //UMat version
+			//image = new Mat("lena.jpg", ImreadModes.Color); //CPU version
+
 			long detectionTime;
 			List<Rectangle> faces = new List<Rectangle>();
+			List<Rectangle> eyes = new List<Rectangle>();
+
+//			DetectFace.Detect(
+//			  image, "haarcascade_frontalface_default.xml", "haarcascade_eye.xml",
+//			  faces, eyes, out detectionTime);
 			DetectFace.DetectOnlyFace(
 			  image, "haarcascade_frontalface_default.xml",
 			  faces, out detectionTime);
 
 			foreach (Rectangle face in faces)
 				CvInvoke.Rectangle(image, face, new Bgr(Color.Red).MCvScalar, 2);
+			foreach (Rectangle eye in eyes)
+				CvInvoke.Rectangle(image, eye, new Bgr(Color.Blue).MCvScalar, 2);
+
+			//display the image 
 			using (InputArray iaImage = image.GetInputArray()) {
 				string sreport = String.Format(
 				   "Completed face and eye detection using {0} in {1} milliseconds",
@@ -94,10 +119,7 @@ namespace CameraCapture {
 				   (iaImage.IsUMat && CvInvoke.UseOpenCL) ? "OpenCL" : "CPU", detectionTime);
 				Console.WriteLine(sreport);
 			}
-		} // ////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-		private void SplitContainer2_Panel2_Paint(object sender, PaintEventArgs e) {
-
 		}
+
 	} // *************************************************************************************************************
 }
